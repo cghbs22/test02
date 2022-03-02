6385b0d6a8af04e0d5fd08300728085fb538968a
cgh <cgh@qq.com>
2022-03-02 16:53:04 +0800

    第一次提交

---
+++ src/main/java/com/baggio/App.java
@@ -0,0 +1,13 @@
+package com.baggio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
