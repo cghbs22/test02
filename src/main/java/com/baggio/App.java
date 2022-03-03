9c62a644bac92a1239b2ca82db4f04d292902967
cgh <cgh@qq.com>
2022-03-03 14:50:56 +0800

    第五次提交，测试合并冲突

--- src/main/java/com/baggio/App.java
+++ src/main/java/com/baggio/App.java
@@ -4,11 +4,10 @@ package com.baggio;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World2!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
+        System.out.println("modify local!");
     }
 }
